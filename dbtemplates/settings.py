aed3ecb5fd1278cfd7598538d63f491c0fc60ebd
Jason Mayfield <jwmayfield@mini.local>
2010-12-13 19:35:57 -0500

    typo

--- dbtemplates/settings.py
+++ dbtemplates/settings.py
@@ -13,7 +13,7 @@ MEDIA_PREFIX = getattr(settings, 'DBTEMPLATES_MEDIA_PREFIX',
 
 USE_REVERSION = getattr(settings, 'DBTEMPLATES_USE_REVERSION', False)
 
-if USE_REVERSION and 'reversion'not in settings.INSTALLED_APPS:
+if USE_REVERSION and 'reversion' not in settings.INSTALLED_APPS:
     raise ImproperlyConfigured("Please add reversion to your INSTALLED_APPS setting to make use of it in dbtemplates.")
 
 USE_CODEMIRROR = getattr(settings, 'DBTEMPLATES_USE_CODEMIRROR', False)
